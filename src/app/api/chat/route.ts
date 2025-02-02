e951bdcc6af1c1b09a36fa486518fac75ceecb8f
Mahmoodreza Zakeri <m.zakeri@jaguars.dev>
2025-02-02 13:30:28 +0330

    .

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -6,13 +6,13 @@ export const dynamic = 'force-dynamic'
 
 export async function POST(req: Request) {
   // Destructure request data
-  const { messages, selectedModel, data } = await req.json()
-  console.log({ messages, selectedModel, data })
+  const { messages, selectedModel, data, systemPrompt } = await req.json()
+  // console.log({ messages, selectedModel, data })
   const ollamaUrl = process.env.NEXT_PUBLIC_OLLAMA_URL || 'http://localhost:11434'
 
   const initialMessages = messages.slice(0, -1)
   const currentMessage = messages[messages.length - 1]
-  console.log(convertToCoreMessages(initialMessages))
+  console.log('systemPrompt', systemPrompt)
 
   const ollama = createOllama({ baseURL: ollamaUrl + '/api' })
 
@@ -30,7 +30,8 @@ export async function POST(req: Request) {
     model: ollama(selectedModel),
     messages: [
       { role: 'system', content: 'Speek in persian' },
-      // { role: "system", content: "You are a helpful developer assistant." },
+      // ...systemPrompt,
+      //i need write another system promp in there with get from request
       ...convertToCoreMessages(initialMessages),
       { role: 'user', content: messageContent }
     ]
